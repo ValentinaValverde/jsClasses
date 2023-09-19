c1e5d127abb41c1e12795be01c6b52284259e3ca
Valentina Valverde <vvgreenville@gmail.com>
2023-09-19 17:15:24 -0400

    Completed all the steps for one element, now the rest will follow.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,15 @@
+//Below are all the HTML elements
+const sonnyInfo = document.getElementById('sonnyInfo');
+const jordanInfo = document.getElementById('jordanInfo');
+
+const sonnyGreet = document.getElementById('sonnyGreet');
+const jordanGreet = document.getElementById('jordanGreet');
+
+const sonnyContact = document.getElementById('sonnyContact');
+const jordanContact = document.getElementById('jordanContact');
+
+const friendsSection = document.getElementById('friendsSection');
+
 //Person work:
 class Person {
 
@@ -27,67 +39,70 @@ class Person {
   
     list_friends(){
       this.friends.map(x => {
-        console.log(this.name + "'s Friends: ", x.name);
-        return (x.name + "'s Friends: ", x.name);
+        // create an el
+        const friends = document.createElement('p');
+
+        friends.innerHTML = (`${this.name}'s Friends: ${x.name}`);
+        return friendsSection.appendChild(friends);
       })
     }
-  
 };
   
-  //Sonny work:
-  const Sonny = new Person('Sonny');
+//Sonny work:
+const Sonny = new Person('Sonny');
   
-  Sonny.email = 'sonny@hotmail.com';
-  Sonny.phone = '483-485-4948';
+Sonny.email = 'sonny@hotmail.com';
+Sonny.phone = '483-485-4948';
   
-  // console.log("Name: ", Sonny.name);
-  // console.log(Sonny);
+// console.log("Name: ", Sonny.name);
+// console.log(Sonny);
   
-  //Jordan work:
-  const Jordan = new Person('Jordan');
-  Jordan.email = 'jordan@aol.com';
-  Jordan.phone = '495-586-3456';
+
+
+//Jordan work:
+const Jordan = new Person('Jordan');
+Jordan.email = 'jordan@aol.com';
+Jordan.phone = '495-586-3456';
   
-  // console.log("Name: ", Jordan.name)
-  // console.log(Jordan);
-  
-  
-  //They're gonna be friends :)
-  // Sonny.greet(Jordan);
-  // Jordan.greet(Sonny);
-  
-  // Sonny.print_contact_info();
-  // Jordan.print_contact_info();
+// console.log("Name: ", Jordan.name)
+// console.log(Jordan);
   
   
-  Sonny.add_friend(Jordan);
-  Jordan.add_friend(Sonny);
+//They're gonna be friends :)
+// Sonny.greet(Jordan);
+// Jordan.greet(Sonny);
   
-  Sonny.num_friends();
-  Jordan.num_friends();
-  
-  Sonny.list_friends();
-  Jordan.list_friends();
+// Sonny.print_contact_info();
+// Jordan.print_contact_info();
   
   
+Sonny.add_friend(Jordan);
+Jordan.add_friend(Sonny);
+  
+Sonny.num_friends();
+Jordan.num_friends();
+
+Sonny.list_friends();
+Jordan.list_friends();
   
   
   
   
-  //Vehicle work:
-  // class vehicle{
-  //   constructor(make, model, year){
-  //     this.make = make
-  //     this.model = model;
-  //     this.year = year;
-  //   }
-  //   print_info() {
-  //     console.log(car.make, car.model, car.year)
-  //   }
-  // };
   
-  // const car = new vehicle('VW', 'GTI', 2018)
-  // console.log(car);
-  // car.print_info();
+//Vehicle work:
+// class vehicle{
+//   constructor(make, model, year){
+//     this.make = make
+//     this.model = model;
+//     this.year = year;
+//   }
+//   print_info() {
+//     console.log(car.make, car.model, car.year)
+//   }
+// };
+  
+// const car = new vehicle('VW', 'GTI', 2018)
+// console.log(car);
+// car.print_info();
   
   
